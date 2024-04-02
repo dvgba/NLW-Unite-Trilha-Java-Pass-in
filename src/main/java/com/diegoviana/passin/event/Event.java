eee3d391d542fa6e9c06a99c9313f25339e26b2d
dvgba <diegovianagomes01@gmail.com>
2024-04-02 06:37:24 -0300

    Criação da Entidade de Evento e Migrations db

---
+++ src/main/java/com/diegoviana/passin/event/Event.java
@@ -0,0 +1,37 @@
+package com.diegoviana.passin.event;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="event")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String details;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false, name = "maximum_attendees")
+    private Integer maximumAttendees;
+}
